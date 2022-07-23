2a4c948640a07dd5449aa1d2cad91fac223f9b3b
Devvy <18744053+KaiDevvy@users.noreply.github.com>
2022-07-23 12:07:40 +0100

    Created a class for managing windows and their data

---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window : MonoBehaviour
+{
+    [Header("References")]
+    public RawImage barIcon;
+
+    public WindowData data;
+    private bool _isHidden = true;
+    private Coroutine _showing;
+    private Coroutine _hiding;
+    private ClickableIcon _taskbarIcon;
+
+    private readonly float _animDuration = 0.2f;
+
+    private void Start()
+    {
+        if (data.showOnTaskbar)
+        {
+            _taskbarIcon = Instantiate(OSManager.instance.iconPrefab, OSManager.instance.taskbarIconPool)
+                .GetComponent<ClickableIcon>();
+            _taskbarIcon.SetLink(this);
+        }
+
+        barIcon.texture = data.icon;
+    }
+
+    public void Show(Vector2 startPos)
+    {
+        // Currently playing this animation, don't touch it
+        if (_showing != null || !_isHidden)
+            return;
+
+        transform.position = startPos;
+        gameObject.SetActive(true);
+
+        _showing = StartCoroutine(ShowCoroutine(startPos));
+    }
+
+    public IEnumerator ShowCoroutine(Vector2 startPos)
+    {
+
+
+        float t = 0;
+        while (t < _animDuration)
+        {
+            float currentTime = (t / _animDuration);
+            transform.position = Vector2.Lerp(startPos, Vector2.zero, currentTime);
+            transform.localScale = Vector3.Lerp(new Vector3(0,0,1), Vector3.one, currentTime);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        _isHidden = false;
+        _showing = null;
+    }
+
+
+    public void Hide() => Hide(_taskbarIcon.transform.position);
+    public void Hide(Vector2 endPos)
+    {
+        if (_hiding != null || _isHidden)
+            return;
+
+        _hiding = StartCoroutine(HideCoroutine(endPos));
+    }
+
+    public IEnumerator HideCoroutine(Vector2 endPos)
+    {
+        Vector2 startScale = transform.localScale;
+        Vector2 startPos = transform.position;
+
+        float t = 0;
+        while (t < _animDuration)
+        {
+            float currentTime = (t / _animDuration);
+            transform.position = Vector2.Lerp(startPos, endPos, currentTime);
+            transform.localScale = Vector3.Lerp(startScale, new Vector3(0,0,1), currentTime);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        _isHidden = true;
+        _hiding = null;
+        
+        gameObject.SetActive(false);
+
+    }
+
+    public void Destroy()
+    {
+
+        Destroy(_taskbarIcon.gameObject);
+        Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/WindowData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "NewWindowData", menuName = "Data/Window")]
+public class WindowData : ScriptableObject
+{
+    public string windowName;
+    public Texture2D icon;
+    public GameObject window;
+    public bool showOnDesktop = true;
+    public bool showOnTaskbar = true;
+
+
+}
